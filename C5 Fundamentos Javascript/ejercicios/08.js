55c91b41444a12e19c07e71f71a16c711c006714
Sergio-Gordillo <semagoma97@gmail.com>
2024-03-11 23:29:25 -0600

    Solucion ejercicios 1 al 9

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,13 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  if(typeof(numero) == "number"){
+    return true
+  }
+  else{
+    return false
+  }
 }
+
 
 module.exports = esNumeroEntero;
--- C5 Fundamentos Javascript/ejercicios/09.js
+++ C5 Fundamentos Javascript/ejercicios/09.js
@@ -3,6 +3,15 @@ function esNuloOIndefinido(valor) {
   // Si este valor es null o undefined, retorna true.
   // De lo contrario, retorna false.
   // Tu código:
+  if(typeof(valor) == "null"){
+    return true
+  }
+  else if(typeof(valor) == "undefined"){
+    return true
+  }
+  else{
+    return false
+  }
 }
 
 module.exports = esNuloOIndefinido;
